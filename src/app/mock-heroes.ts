4f6ddb0f97e119b29cd286490a55c417e2051e18
Aldo <aldoe.profuturo@gmail.com>
2020-05-23 03:39:03 -0500

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Deku' },
+  { id: 11, name: 'Uravity' },
+  { id: 12, name: 'Shoto' },
+  { id: 13, name: 'Froppy' },
+  { id: 14, name: 'Tsukuyomi' },
+  { id: 15, name: 'Grape Juice' },
+  { id: 16, name: 'Creati' },
+  { id: 17, name: 'Pinky' },
+  { id: 18, name: 'Ingenium' },
+  { id: 19, name: 'Earphone Jack' },
+  { id: 20, name: 'Red Riot' }
+];
